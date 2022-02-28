3a20227fea15bd8c89e70598719cc5fce713ba80
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-02-28 14:48:42 -0300

    types

---
+++ types.ts
@@ -0,0 +1,36 @@
+//boolean
+let isOpen: boolean;
+isOpen = true;
+
+//string
+let message: string;
+message = `foo ${isOpen ? "open" : "closed"}`;
+
+//Number
+let total: Number;
+total = 20;
+total = 20.3;
+
+//array(type[])
+let items: number[];
+items = [1, 2, 3, 5, 4, 8, 7];
+//generic types
+let values: Array<number>;
+values = [1, 2, 3, 4, 5, 6];
+
+//tuple(type) [type, type, type]
+let titles: [number, string, number];
+titles = [1, "foo", 3232];
+let subTitles: [number, string, number, number, string, number];
+subTitles = [1, "foo", 3232, 1, "foo", 3232];
+
+//enumm
+enum Colors {
+  white = "#ffff",
+  black = "#0000",
+}
+
+//any (not cool )
+let coisa: any 
+coisa =true;
+coisa = []
